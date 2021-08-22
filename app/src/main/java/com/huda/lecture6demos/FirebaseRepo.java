3e2daa7e0b57ae781612621d6ebad23cb540675d
HudaAnwar <HudaAnwar@users.noreply.github.com>
2021-08-23 01:28:49 +0200

    lecture6 commit

---
+++ app/src/main/java/com/huda/lecture6demos/FirebaseRepo.java
@@ -0,0 +1,4 @@
+package com.huda.lecture6demos;
+
+public class FirebaseRepo {
+}
---
+++ app/src/main/java/com/huda/lecture6demos/LoginViewModel.java
@@ -0,0 +1,7 @@
+package com.huda.lecture6demos;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+
+}
